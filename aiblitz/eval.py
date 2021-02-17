2c2eeef59ddbfe7c94f7aecdc5d8f7b600aec63e
Gaurang Tandon <gaurang.tandon@students.iiit.ac.in>
2021-02-17 12:45:43 +0530

    Fixed the checker There is occassional error of exception parsing from PV but it's logged not raised so it's hopefully fine Estimated time to finish is roughly 6hours maybe we can parallelize it :3

--- aiblitz/eval.py
+++ aiblitz/eval.py
@@ -1,20 +1,62 @@
 import chess.engine
+import tqdm
 
+def engine_init():
+    return chess.engine.SimpleEngine.popen_uci("./stockfish")
+
+def make_fen_valid(fen, move):
+    fen += " "
+    fen += move
+    fen += " - - 0 10"
+    return fen
+
+VALID_MOVES = ["b", "w"]
+
+def get_board(fen, move):
+    try:
+        return chess.Board(make_fen_valid(fen, move))
+    except Exception as e:
+        # print(e)
+        return chess.Board(chess.STARTING_BOARD_FEN)
 
 def evaluate(fen_list):
     results = []
-    for fen, turn in fen_list:
-        engine = chess.engine.SimpleEngine.popen_uci("./stockfish")
-        position = chess.Board(fen)
-        # Path to executable, get it from here https://stockfishchess.org/download/
-        info = engine.analyse(position, chess.engine.Limit(time=0.1))
-        # get a relative integer score from white's perspective, the score is in centi-pawns
-        score = info["score"].white().score(mate_score=10000)
-        print("Score:", score, fen)
-        # As per AICrowd, one of the sides is certainly winning/mating
-        # If this assertion fails, then the engine had insufficient time to analyze the position
-        assert(abs(score) >= 150)
-        # Print winner side
-        results.append("white" if score > 0 else "black")
+    engine = engine_init()
+
+    fen_list = list(fen_list)
+    for fen, _turn in tqdm.tqdm(fen_list):
+        winner = None
+        positions = [get_board(fen, move) for move in VALID_MOVES]
+
+        for move, position in zip(VALID_MOVES, positions):
+            if position.is_checkmate():
+                winner = "w" if move == "b" else "b"
+                break
+
+        if winner is None:
+            scores = []
+
+            for move, position in zip(VALID_MOVES, positions):
+                try:
+                    # Path to executable, get it from here https://stockfishchess.org/download/
+                    info = engine.analyse(position, chess.engine.Limit(time=1))
+
+                    # get a relative integer score from white's perspective, the score is in centi-pawns
+                    score = info["score"].white().score(mate_score=10000)
+
+                    # print("Score:", score, fen)
+                    scores.append(abs(score))
+                except chess.engine.EngineTerminatedError:
+                    engine = engine_init()
+                    scores.append(0)
+                    # print(e)
+                    # print("This move is incorrect")
+
+            winner = VALID_MOVES[0] if scores[0] > scores[1] else VALID_MOVES[1]
+
+        assert (winner is not None)
+        results.append(winner)
+
+    engine.quit()
 
     return results
